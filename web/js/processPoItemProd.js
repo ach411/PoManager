695e567066283deaed9c36868addee30757c3853
ach411 <ach411@hotmail.fr>
2015-05-18 15:48:05 -0700

    add the ability to modify comment in process prod page

--- web/js/processPoItemProd.js
+++ web/js/processPoItemProd.js
@@ -55,4 +55,17 @@ $(function() {
 		}
 	});
 	
+	//update comment
+	$('[id*="update-comment-id"]').click(function(event) {
+		// get item number where event comes from
+		var re = /\d+/ig;
+		var result = re.exec(event.target.id);
+		var index = result[0];
+		//var text = $('[id*="comment-text-id"]').val();
+		var text = $('#comment-text-id-'+index).val();
+		$.post("../../update/poitem/"+index, { comment: text});
+		alert("Comment updated: "+text);
+		
+	});
+	
 });
